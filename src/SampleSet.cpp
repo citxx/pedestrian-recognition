a210593cf61c0f5bf14e8e647265af132ece0281
Artem Tabolin <artemtab@yandex.ru>
2011-10-01 18:56:25 +0400

    The definition of Sample set is added

---
+++ src/SampleSet.cpp
@@ -0,0 +1,1 @@
+#include "SampleSet.hpp"
---
+++ src/SampleSet.hpp
@@ -0,0 +1,12 @@
+#ifndef SAMPLE_SET_HPP
+#define SAMPLE_SET_HPP
+
+#include <QDir>
+
+class SampleSet {
+  public:
+    SampleSet(const QDir &directory);
+
+};
+
+#endif  // SAMPLE_SET_HPP
