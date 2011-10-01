d0051b0cf324a4f124897a30946bd8d1daf6ca61
Artem Tabolin <artemtab@yandex.ru>
2011-10-01 17:39:13 +0400

    Working modes are defined

---
+++ src/runModes.hpp
@@ -0,0 +1,9 @@
+#ifndef RUN_MODES_HPP
+#define RUN_MODES_HPP
+
+void runLearningMode(const char *classifierFileName, const char *sampleSetDirectoryName, const char *coincidenceFileName);
+void runClassifyingMode(const char *classifierFileName, const char *sampleSetDirectoryName);  // TODO: arguments
+void runGraphicsMode();  // TODO: arguments
+
+#endif  // RUN_MODES_HPP
+
