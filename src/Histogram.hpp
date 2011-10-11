4bf520e7f53cbe315ebd0383ca45528541fa2573
Artem Tabolin <artemtab@yandex.ru>
2011-10-11 22:35:50 +0400

    Class 'Sample' is implemented

--- src/Histogram.hpp
+++ src/Histogram.hpp
@@ -12,6 +12,16 @@ Histogram <size> operator +(const Histogram <size> &a, const Histogram <size> &b
     for (unsigned int i = 0; i < size; i++) {
         answer.data[i] = a.data[i] + b.data[i];
     }
+    return answer;
+}
+
+template <unsigned int size>
+Histogram <size> operator -(const Histogram <size> &a, const Histogram <size> &b) {
+    Histogram <size> answer;
+    for (unsigned int i = 0; i < size; i++) {
+        answer.data[i] = a.data[i] - b.data[i];
+    }
+    return answer;
 }
 
 template <unsigned int size>
@@ -35,7 +45,29 @@ class Histogram {
         return std::vector <int>(this->data, this->data + size);
     }
 
+    int & operator [](unsigned int index) {
+        if (index < 0 || size <= index) {
+            throw "Histogram: wrong index";
+        }
+        return data[index];
+    }
+
+    const int & operator [](unsigned int index) const {
+        if (index < 0 || size <= index) {
+            throw "Histogram: wrong index";
+        }
+        return data[index];
+    }
+
+    Histogram <size> & operator =(const Histogram <size> &hist) {
+        for (unsigned int i = 0; i < size; i++) {
+            this->data[i] = hist.data[i];
+        }
+        return *this;
+    }
+
     friend Histogram <size> operator + <size>(const Histogram <size> &a, const Histogram <size> &b);
+    friend Histogram <size> operator - <size>(const Histogram <size> &a, const Histogram <size> &b);
 };
 
 #endif  // HISTOGRAM_HPP
---
+++ src/Sample.hpp
@@ -0,0 +1,21 @@
+#ifndef SAMPLE_HPP
+#define SAMPLE_HPP
+
+#include <vector>
+#include <opencv/cv.h>
+
+#include "Histogram.hpp"
+
+const int HISTOGRAM_SIZE = 8;
+
+class Sample {
+    std::vector <std::vector <Histogram <HISTOGRAM_SIZE> > > histogram;
+
+  public:
+    Sample(cv::Mat image);
+
+    int maximalShift() const;
+    std::vector <double> getDescriptor(int x) const;
+};
+
+#endif  // PEDESTRIAN_SAMPLE_HPP
